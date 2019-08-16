fc0ca21653e5d75926f3491934a5bacd1ee2a586
oliverochman <oliver.ochman@hotmail.com>
2019-08-16 11:32:09 +0200

    refactor reg_key model with secure token and add creation of reg_keys on uni sign up

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,13 +1,12 @@
 class RegistrationsController < ::DeviseTokenAuth::RegistrationsController
-  def render_create_success
-    
-    binding.pry
-        
-
+  def render_create_success    
+    5.times { RegistrationKey.create(user: @resource) }
+    @resource.reload
 
     render json: {
       status: 'success',
       data: {
+        registration_keys: @resource.registration_keys,
         user: resource_data
       }
     }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_16_081427) do
+ActiveRecord::Schema.define(version: 2019_08_16_090453) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2019_08_16_081427) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["combination"], name: "index_registration_keys_on_combination", unique: true
     t.index ["user_id"], name: "index_registration_keys_on_user_id"
   end
 
