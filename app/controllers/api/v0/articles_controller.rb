78b2139f4fd8080c6a65d9373468ad004646dc9d
oliverochman <oliver.ochman@hotmail.com>
2019-08-20 12:02:07 +0200

    add functionality to create action in articles controller

--- app/controllers/api/v0/articles_controller.rb
+++ app/controllers/api/v0/articles_controller.rb
@@ -1,10 +1,25 @@
 class Api::V0::ArticlesController < ApplicationController
+  before_action :authenticate_api_v0_user!, only: [:create]
+
   def index
     articles = Article.all
     render json: articles, each_serializer: Articles::IndexSerializer
   end
 
   def create
+    article = current_api_v0_user.articles.create(article_params)
     
+    if article.persisted?
+      render json: { message: 'all good' } 
+    else
+      render json: { message: 'not good' } 
+    end
+    
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body, :author)
   end
 end
