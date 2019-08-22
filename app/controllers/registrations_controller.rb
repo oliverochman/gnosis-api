b9b783cc102591070f51c905a7bfa0b91b564c12
oliverochman <oliver.ochman@hotmail.com>
2019-08-22 12:14:56 +0200

    adds research group conditionals in create action for registrations controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,12 +1,24 @@
 class RegistrationsController < ::DeviseTokenAuth::RegistrationsController
 
   def create
-    
-    binding.pry
+    if params[:role] === 'research_group'
+      reg_key = RegistrationKey.find_by(combination: params[:sign_up_registration_key])
+      unless reg_key.nil?
+        
+        binding.pry
+        
+      else
+        render json: {
+          status: 'error',
+          data:   resource_data,
+          errors: 'Invalid registartion key'
+        }, status: 422
+      end
+    end
     
     super
   end
-  
+
   def render_create_success
     if @resource.role === 'university'  
       5.times { RegistrationKey.create(user: @resource) }
