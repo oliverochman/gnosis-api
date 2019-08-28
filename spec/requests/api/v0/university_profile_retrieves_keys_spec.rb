6b8102771b301fb3e0bc704c050d6cbd5d3ed2ca
seth <sethnejame@gmail.com>
2019-08-28 14:10:12 +0200

    REGISTRATION KEYS AHHHHH

--- spec/requests/api/v0/university_profile_retrieves_keys_spec.rb
+++ spec/requests/api/v0/university_profile_retrieves_keys_spec.rb
@@ -1,9 +1,9 @@
-RSpec.describe 'University Profile retrieves research keys', type: :request do
+RSpec.describe 'University Profile retrieves registration keys', type: :request do
   let(:university) { create(:user, role: :university, subscriber: true) }
   let(:credentials) { university.create_new_auth_token }
   let(:headers) { { HTTP_ACCEPT: 'application/json' }.merge!(credentials) }
 
-  describe 'research keys are returned for subscribed user' do
+  describe 'registration keys are returned for subscribed user' do
     before do
       5.times { RegistrationKey.create(user: university) }
       get "/api/v0/users/#{university.id}", headers: headers
@@ -13,7 +13,7 @@ RSpec.describe 'University Profile retrieves research keys', type: :request do
       expect(response.status).to eq 200
     end
     
-    it 'returns 5 research keys' do
+    it 'returns 5 registration keys' do
       expect(response_json.count).to eq 5
     end
   end
